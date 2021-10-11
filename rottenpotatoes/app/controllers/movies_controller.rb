05043cccbf1ea43eff5ccbbdbb8d1e2caeb7366b
EC2 Default User <ec2-user@ip-172-31-21-90.us-east-2.compute.internal>
2021-10-11 00:02:54 +0000

    commit 2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -34,6 +34,15 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+  
+  def search
+    @similar_movies = Movie.similar_movies(params[:title])
+    if @similar_movies.nil?
+      flash[:notice]="'#{params[:title]}' has no director info"
+      redirect_to movies_path#, alert: "'#{params[:title]}' has no director info"
+    end
+    @movie = Movie.find_by(title: params[:title])
+  end
 
   def destroy
     @movie = Movie.find(params[:id])
--- rottenpotatoes/db/migrate/20211010213704_add_director_to_movies.rb
+++ rottenpotatoes/db/migrate/20211010213704_add_director_to_movies.rb
@@ -1,4 +1,3 @@
-
 class AddDirectorToMovies < ActiveRecord::Migration
   def change
     add_column :movies, :director, :string
